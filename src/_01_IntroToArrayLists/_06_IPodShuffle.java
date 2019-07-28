b7c4019b917f6eb3a1a30f5180ddbbf411e447f9
league <league@imac-14.attlocal.net>
2019-07-28 13:00:48 -0700

    i pod shuffle, integer stack

--- src/_01_IntroToArrayLists/_06_IPodShuffle.java
+++ src/_01_IntroToArrayLists/_06_IPodShuffle.java
@@ -1,21 +1,38 @@
 package _01_IntroToArrayLists;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 
 //Copyright The League of Amazing Programmers, 2015
 
-public class _06_IPodShuffle{
+public class _06_IPodShuffle implements ActionListener{
+	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button = new JButton();
+	ArrayList<Song> songs = new ArrayList<Song>();	
+	Song s = new Song("demo.mp3");
+	Song l = new Song("lh.mp3");
+	Song m = new Song("sm.mp3");
+	Song k = new Song("tk.mp3");
+	
+	
 	public _06_IPodShuffle() {
 		// 1. Use the Song class the play the demo.mp3 file.
-		Song s = new Song("demo.mp3");
-		s.play();
-				
+
+		songs.add(s);
+		songs.add(l);
+		songs.add(m);
+		songs.add(k);
+		
 		/**
 		 * 2. Congratulations on completing the sound check! * Now we want to make an
 		 * iPod Shuffle that plays random music. * Create an ArrayList of Songs and a
@@ -24,25 +41,50 @@ public class _06_IPodShuffle{
 		 * subsequent button clicks.
 		 */
 		
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
-		JButton button = new JButton();
-		JButton stop = new JButton();
+
 		frame.add(panel);
-		panel.add(stop);
 		panel.add(button);
 		frame.setVisible(true);
 		button.setText("Surprise me!");
-		stop.setText("Stop");
+		button.addActionListener(this);
+
+	}
+	public static void main(String[] args) {
+		_06_IPodShuffle ipod = new _06_IPodShuffle();
 		
 	}
-	
-	public static void main(String[] args) {
-		new _06_IPodShuffle();
-		ArrayList<Song> songs = new ArrayList<Song>();
 		
-		for (int i = 0; i < songs.size(); i++) {
-			System.out.println(i);
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource() == button) {
+			for (int i = 0; i < songs.size(); i++) {
+				songs.get(i).stop();
+			}
+			Random r = new Random();
+			int rand = r.nextInt(songs.size());
+			for (int i = 0; i < songs.size(); i++) {
+				if(i == rand) {
+					songs.get(i).play();
+					if(songs.get(i).equals(s)) {
+						JOptionPane.showMessageDialog(null, "Playing demo song");
+					}
+					else if(songs.get(i).equals(m)) {
+						JOptionPane.showMessageDialog(null, "Playing Walkin' On The Sun by Smash Mouth");
+					}
+					else if(songs.get(i).equals(l)) {
+						JOptionPane.showMessageDialog(null, "Playing Mrs. Robinson by The Lemonheads");
+					}
+					else if(songs.get(i).equals(k)) {
+						JOptionPane.showMessageDialog(null, "Playing Spaceman by The Killers");
+					}
+					
+				}
+			}
 		}
+		
+		
+		
+		
 	}
 }
