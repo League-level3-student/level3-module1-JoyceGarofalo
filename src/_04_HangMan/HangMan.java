7f373a6f3b04ebe0b13bf4c0b447e70a7a3e81e7
league <league@imac-14.attlocal.net>
2019-08-26 16:29:04 -0700

    hang man ip

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -17,7 +17,11 @@ public class HangMan implements KeyListener{
 	JLabel label = new JLabel();
 	JLabel lives = new JLabel();
 	JLabel guesses = new JLabel();
+	JFrame end = new JFrame();
+	JPanel endp = new JPanel();
+	JLabel endl = new JLabel();
 	int life = 10;
+	int lettersGuessed = 0;
 	Boolean guessiscorrect;
 	static Stack<String> words = new Stack<String>();
 	static ArrayList<String> underscores = new ArrayList<String>();
@@ -28,12 +32,15 @@ public class HangMan implements KeyListener{
 		panel.add(label);
 		panel.add(lives);
 		panel.add(guesses);
+		end.add(endp);
+		endp.add(endl);
+		endl.setText("Game Over");
+		end.setSize(200,200);
 		guesses.setText("guess any letter");
 		lives.setText("lives: " + life);
 		frame.setSize(200,200);
 		frame.addKeyListener(this);
 		
-		//label.add('_' * words.size());
 	}
 	
 	public static void main(String[] args) {
@@ -41,16 +48,20 @@ public class HangMan implements KeyListener{
 		hm.setup();
 	}
 	
-	public void setup() {
+	public void setup() { //picks words
 		String word = JOptionPane.showInputDialog("Welcome to Hangman! \n How many words would you like to guess?");
 		int num = Integer.parseInt(word);
 		for (int i = 0; i < num; i++) {
 			String s = Utilities.readRandomLineFromFile("dictionary.txt");
 			words.add(s);
-			
 		}
+		wordSetup();
+
+	}
+	
+	public void wordSetup() { //sets up frame
 		currentWord = words.pop();
-		//System.out.println(currentWord);
+		System.out.println(currentWord);
 		for (int j = 0; j < currentWord.length(); j++) {
 			underscores.add("_");
 			
@@ -61,9 +72,10 @@ public class HangMan implements KeyListener{
 		
 		labelText();
 		
-		
-		
 	}
+	
+	
+	
 	
 	
 	public void labelText() { //sets the label to underscores and fills with letters
@@ -84,16 +96,32 @@ public class HangMan implements KeyListener{
 				underscores.set(i, guess + "");
 				guessiscorrect = true;
 			}
+			
 		
 		}
+		
 		if(guessiscorrect == false) {
 			life -= 1;
 			lives.setText("lives: " + life);
 		}
 		else {
-			
+			lettersGuessed += 1;					//adds repeating letters which is not good
+			System.out.println(lettersGuessed);
 		}
 		labelText();
+			
+		if(lettersGuessed == currentWord.length() && life > 0) { //changes to next word
+			
+			//System.out.println(label);
+			life = 10;
+			//label.setText("");
+			wordSetup();
+		}
+		
+		else if(life == 0) {
+			frame.setVisible(false);
+			end.setVisible(true);
+		}
 	}
 
 	
