1d2aa0665470fa60c35fd6392c0951207bfd1d86
league <league@imac-14.attlocal.net>
2019-09-15 17:32:04 -0700

    intro to hashmaps, log search

--- src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
+++ src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
@@ -1,7 +1,17 @@
 package _06_Intro_To_Hash_Maps;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 
-public class _02_LogSearch {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import _04_HangMan.HangMan;
+
+public class _02_LogSearch implements ActionListener{
   /* 
 	 * Crate a HashMap of Integers for the keys and Strings for the values.
 	 * Create a GUI with three buttons. 
@@ -29,5 +39,75 @@ public class _02_LogSearch {
 	 * 				is not in the list. 
 	 *
 	 * */
+	HashMap<Integer, String> hm = new HashMap<Integer, String>();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton add = new JButton();
+	JButton search = new JButton();
+	JButton view = new JButton();
+	String name;
 	
+	public static void main(String[] args) {
+		_02_LogSearch ls = new _02_LogSearch();
+		ls.LogSearch();
+	}
+	
+	public void LogSearch() {
+		add.addActionListener(this);
+		search.addActionListener(this);
+		view.addActionListener(this);
+		
+		frame.add(panel);
+		panel.add(add);
+		panel.add(search);
+		panel.add(view);
+		frame.setSize(200,200);
+		frame.setVisible(true);
+		add.setText("add");
+		search.setText("search");
+		view.setText("view");
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource() == add) {
+			String id = JOptionPane.showInputDialog("enter an ID number");
+			int ID = Integer.parseInt(id);
+			name = JOptionPane.showInputDialog("enter a name");
+			hm.put(ID, name);
+		}
+		
+		if(e.getSource() == search) {
+			String h = JOptionPane.showInputDialog("enter an ID number");
+			int IDsearch = Integer.parseInt(h);
+
+			if(hm.get(IDsearch) != null) {
+				JOptionPane.showMessageDialog(null, hm.get(IDsearch));
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "entry does not exist");
+			}
+			
+			
+			
+		}
+		
+		if(e.getSource() == view) {
+			String s = "";
+			for(int key: hm.keySet()) {
+				s +=  "ID: " + key + " Name: " + hm.get(key) + "\n";
+				
+			}
+			JOptionPane.showMessageDialog(null, s);
+
+			
+		}
+		
+		
+		
+		
+	}
+
 }
